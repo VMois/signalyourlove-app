7d4da654074522010999595525fbc83ad0bca2de
VMois <vmois@protonmail.com>
2023-01-13 16:23:07 +0100

    Add total number of calls made
    closes #1

--- src/Display.tsx
+++ src/Display.tsx
@@ -17,19 +17,25 @@ function Display({ statistics }) {
                 <div className="shadow p-4 col-span-2">
                     <p className="text-indigo-500 text-base font-medium uppercase leading-4">We have been writing to each other endlessly for</p>
                     <p className="text-black font-bold text-2xl inline-flex items-center space-x-2 mt-2">
-                    <span>{statistics.total_days} days</span>
+                        <span>{statistics.total_days} days</span>
                     </p>
                 </div>
-                <div className="shadow p-4 col-span-2">
+                <div className="shadow p-4">
                     <p className="text-indigo-500 text-base font-medium uppercase leading-4">We sent each other</p>
                     <p className="text-black font-bold text-2xl inline-flex items-center space-x-2 mt-2">
-                    <span>{statistics.total_messages} messages</span>
+                        <span>{statistics.total_messages} messages</span>
+                    </p>
+                </div>
+                <div className="shadow p-4">
+                    <p className="text-indigo-500 text-base font-medium uppercase leading-4">We called each other</p>
+                    <p className="text-black font-bold text-2xl inline-flex items-center space-x-2 mt-2">
+                        <span>{statistics.total_calls} times</span>
                     </p>
                 </div>
                 <div className="shadow p-4 col-span-2">
                     <p className="text-indigo-500 text-base font-medium uppercase leading-4">Our record is</p>
                     <p className="text-black font-bold text-2xl inline-flex items-center space-x-2 my-2">
-                    <span>{statistics.top_day.total} messages on {statistics.top_day.date}</span>
+                        <span>{statistics.top_day.total} messages on {statistics.top_day.date}</span>
                     </p>
                 </div>
                 </div>
