a7e3a0873371af00002ba1abc8f60a6bda1e1ea5
VMois <vmois@protonmail.com>
2023-02-08 18:53:14 -0500

    Add footer with website link and description to SelectConversation view
    - make default browser open when external link is clicked
    
    closes #24

--- src/SelectConversation.tsx
+++ src/SelectConversation.tsx
@@ -17,7 +17,7 @@ export default function SelectConversation({ conversations, onSelect }: { conver
 
     return (
         <div className="mt-6 px-24 container mx-auto">
-            <div className="mt-24 mb-12">
+            <div className="mt-12 mb-4 lg:mt-24 lg:mb-12">
                 <h1 className="text-rose-500 text-center text-6xl font-bold">
                 Welcome!
                 </h1>
@@ -25,8 +25,8 @@ export default function SelectConversation({ conversations, onSelect }: { conver
                     It is time to signal your love
                 </h2>
             </div>
-            <div className="max-w-2xl mx-auto">
-                <label htmlFor="countries" className="block mt-24 mb-2 text-1xl font-bold text-gray-700">Select a conversation</label>
+            <div className="mt-12 lg:mt-24 max-w-2xl mx-auto">
+                <label htmlFor="countries" className="block mb-2 text-1xl font-bold text-gray-700">Select a conversation</label>
                 <select value={conversationId} onInput={onInput} id="conversations" className="border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5">
                     {conversations.map(function(convo, _){
                         return (
@@ -38,6 +38,12 @@ export default function SelectConversation({ conversations, onSelect }: { conver
             <button onClick={() => onSelect(conversationId)} className="mt-12 block uppercase mx-auto shadow bg-rose-500 hover:bg-rose-400 focus:shadow-outline focus:outline-none text-white text-1xl py-3 px-10 rounded">
                 Show statistics
             </button>
+            <div className="mt-12 lg:mt-24">
+                <h2 className="text-center mb-4 text-1xl text-gray-700">
+                    Visit <a className="text-rose-500" target="_blank" href="https://signalyourlove.app">signalyourlove.app</a> for more information
+                </h2>
+                <h2 className="text-center text-1xl text-gray-700">Created by Vladyslav Moisieienkov</h2>
+            </div>
         </div>
     );
 }
